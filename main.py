0a035805a4922e4d29cbd78bddd822e3b4a0c86f
leithalani <leithalani@ufl.edu>
2023-10-25 16:27:10 -0400

    main method

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("HEllo")
+def encode(password):
+    for i in range(0, len(password)):
+
+
+if __name__ == "__main__":
+    while True:
+        print("Menu\n-------------")
+        print("1. Encode\n2. Decode\n3. Quit")
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+
+
+
