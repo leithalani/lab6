79d6fd12d4447dd5d5e31b6e5d830387830679c6
leithalani <leithalani@ufl.edu>
2023-10-25 16:58:44 -0400

    added encode(password)

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
         if option == 1:
             password = input("Please enter your password to encode: ")
             enc_password = encode(password)
+            print("Your password has been encoded and stored!")
 
         elif option == 2:
             print(f"The encoded password is {enc_password}, and the original password is .")
